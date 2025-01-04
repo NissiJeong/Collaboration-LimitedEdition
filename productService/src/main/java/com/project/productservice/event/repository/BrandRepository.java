44ffb5407d30d47d4e202de5c4c19ac55e86edb0
NissyJeong <jnissi92@gmail.com>
2025-01-04 15:16:41 +0900

    [refactor] product 관리 간소화

--- productService/src/main/java/com/project/productservice/event/repository/BrandRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.productservice.event.repository;
-
-import com.project.productservice.event.entity.Brand;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BrandRepository extends JpaRepository<Brand, Long> {
-}
--- productService/src/main/java/com/project/productservice/event/repository/CollaborationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.productservice.event.repository;
-
-import com.project.productservice.event.entity.Collaboration;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CollaborationRepository extends JpaRepository<Collaboration, Long> {
-}
